e5293c711be3323151fef16b474076244e23217f
VinyMT <viniciusmatiolatramontin@gmail.com>
2022-01-03 20:18:06 -0300

    Created Product class and other related classes

---
+++ src/main/java/com/vinymt/course/resources/ProductResource.java
@@ -0,0 +1,33 @@
+package com.vinymt.course.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vinymt.course.entities.Product;
+import com.vinymt.course.services.ProductService;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductResource {
+	
+	@Autowired
+	private ProductService service;
+	
+	@GetMapping
+	public ResponseEntity<List<Product>> findAll(){
+		List<Product> products = service.findAll();
+		
+		return ResponseEntity.ok().body(products);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Product> findById(@PathVariable Long id) {
+		return ResponseEntity.ok().body(service.findById(id));
+	}
+}
